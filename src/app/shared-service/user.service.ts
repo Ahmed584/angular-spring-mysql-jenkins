a707023e88223b023b13c6d5daf6cf59cb7afcb3
Ahmed584 <32179734+Ahmed584@users.noreply.github.com>
2019-10-01 10:35:51 +0200

    Update user.service.ts

--- src/app/shared-service/user.service.ts
+++ src/app/shared-service/user.service.ts
@@ -9,7 +9,7 @@ import { User } from '../modele/user';
 @Injectable()
 export class UserService {
 
-  private baseUrl:string='http://localhost:8080';
+  private baseUrl:string='http://192.168.1.207:8081';
   private user = new User();
   
 
